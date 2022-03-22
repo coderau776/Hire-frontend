c2aaaa51f71f0b28c5c133ae3315ddb4a1c59a4a
amankumar-21 <kumara_2@rknec.edu>
2022-03-22 14:21:03 +0530

    date comparision

--- src/app/components/interviewer/slot-view/schedule/schedule.component.ts
+++ src/app/components/interviewer/slot-view/schedule/schedule.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleComponent implements OnInit {
 
-  selecteddate=new Date(2022,3,29)
+  selecteddate=new Date(2022,3,24)
 
  events= [{"id":1,"Dates":new Date(2022,3,24),"Time":"8:00 AM",task:"Java interview"},
   {"id":2,"Date":new Date(2022,3,24),"Time":"10:00 AM",task:"Angular interview"},
@@ -20,9 +20,8 @@ export class ScheduleComponent implements OnInit {
   {"id":7,"Date":new Date(2022,3,28),"Time":"8:00 AM",task:"Java interview"},
   {"id":8,"Date":new Date(2022,3,29),"Time":"8:00 AM",task:"SQL interview"},
   {"id":9,"Date":new Date(2022,3,29),"Time":"11:00 AM",task:"Nodejs interview"},
-  {"id":10,"Date":new Date(2022,3,30),"Time":"9:00 AM",task:"Hibernate interview"}]
-
- 
+  {"id":10,"Date":new Date(2022,3,29),"Time":"9:00 AM",task:"Hibernate interview"}]
+  
 
   constructor() {   
   }
