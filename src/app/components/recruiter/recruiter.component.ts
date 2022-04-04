4b2134d45a4cb798e9bf676567d4e24de414ab30
AyushAsawa0003 <77845546+AyushAsawa0003@users.noreply.github.com>
2022-04-04 16:20:10 +0530

    recruiter screen completed

--- src/app/components/recruiter/recruiter.component.ts
+++ src/app/components/recruiter/recruiter.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-recruiter',
   templateUrl: './recruiter.component.html',
-  styleUrls: ['./recruiter.component.css']
+  styleUrls: ['./recruiter.component.css'],
 })
 export class RecruiterComponent implements OnInit {
+  rounds;
+  skills;
+  dateRange;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getRounds(val: any) {
+    this.rounds = val;
   }
 
+  getSkills(val: any) {
+    this.skills = val;
+  }
+
+  getDateRange(val: any) {
+    this.dateRange = val;
+  }
 }
--- src/app/services/slot-view.service.ts
+++ src/app/services/slot-view.service.ts
@@ -4,45 +4,38 @@ import { Observable } from 'rxjs';
 import { Slot } from '../models/Slot';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SlotViewService {
+  private url_slot: string =
+    'http://test-env.eba-p9gye2ye.us-east-1.elasticbeanstalk.com/slots/';
+  slots: Slot[];
+  headers = { 'content-type': 'application/json' };
+  constructor(private http: HttpClient) {}
 
-  private url_slot:string = "http://test-env.eba-p9gye2ye.us-east-1.elasticbeanstalk.com/slots/"
-  slots:Slot[];
-  headers = { 'content-type': 'application/json'};
-  constructor(private http:HttpClient) { }
-
-  getSlots(){
-    return this.http.get<Slot[]>(this.url_slot+localStorage.getItem('id'));
+  getSlots() {
+    return this.http.get<Slot[]>(this.url_slot + localStorage.getItem('id'));
   }
 
-  setSlots(slots:Slot[]){
-    this.slots=slots;
+  setSlots(slots: Slot[]) {
+    this.slots = slots;
   }
 
-  provideSlot(start:string,end:string,date:string):Observable<any>{
-    
+  provideSlot(start: string, end: string, date: string): Observable<any> {
     //validation remaiining
 
-      const body = this.createSlot(start,end,date);
-      return this.http.post(this.url_slot,body,{'headers':this.headers});
-
-   
-    
+    const body = this.createSlot(start, end, date);
+    return this.http.post(this.url_slot, body, { headers: this.headers });
   }
-  createSlot(start: string, end: string,date: string) {
+  createSlot(start: string, end: string, date: string) {
     var slot = new Slot();
     slot.date = date;
     slot.endTime = end;
     slot.startTime = start;
     slot.userId = Number(localStorage.getItem('id'));
-    // available or unavailable 
-    slot.status = "available";
+    // available or unavailable
+    slot.status = 'available';
     this.slots.push(slot);
     return JSON.stringify(slot);
   }
-  
 }
-
-
