a4aad225762547917fa17749e4a73005ac2eb9e7
AyushAsawa0003 <77845546+AyushAsawa0003@users.noreply.github.com>
2022-04-07 14:16:21 +0530

    recruiter screen completed

---
+++ src/app/models/Table.ts
@@ -0,0 +1,10 @@
+export interface Table {
+  slotId: number;
+  title: String;
+  emailId: String;
+  roundAlloted: String;
+  primarySkill: String;
+  secondarySkill: String;
+  tertiarySkill: String;
+  date: String;
+}
