0b7b01e553ff27b1fb4dc04d43e67591707b3d72
coderau776 <atharvauplanchiwar776@gmail.com>
2022-04-19 11:10:21 +0530

    before pull

--- src/app/services/calendar-service.service.spec.ts
+++ src/app/services/calendar-service.service.spec.ts
@@ -1,24 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CalendarServiceService } from './calendar-service.service';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+
 import { HttpClient } from '@angular/common/http';
-import { RouterTestingModule } from '@angular/router/testing';
+
 
 describe('CalendarServiceService', () => {
   let service: CalendarServiceService;
-  let http: HttpClient;
-  let httpController: HttpTestingController;
+  
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [ HttpClientTestingModule, RouterTestingModule ]
-    });
-    service = TestBed.inject(CalendarServiceService);
-    http =  TestBed.inject(HttpClient);
-    httpController =  TestBed.inject(HttpTestingController);
 
-  });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
--- src/app/services/slot-view.service.spec.ts
+++ src/app/services/slot-view.service.spec.ts
@@ -1,16 +1,25 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {HttpClient, HttpClientModule} from '@angular/common/http'
 
 import { SlotViewService } from './slot-view.service';
 
 describe('SlotViewService', () => {
   let service: SlotViewService;
+  let httpMock:HttpTestingController;
+  let http:HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], 
+      providers: [SlotViewService]
+    });
     service = TestBed.inject(SlotViewService);
+    httpMock = TestBed.get(HttpTestingController);
   });
 
   it('should be created', () => {
+    
     expect(service).toBeTruthy();
   });
 });
